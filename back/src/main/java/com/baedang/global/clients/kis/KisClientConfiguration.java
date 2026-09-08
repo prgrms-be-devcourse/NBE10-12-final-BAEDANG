1e34eed5659070f4de580e84601a0a7727e7cec6
SOL4R1S <mingh662@gmail.com>
2026-09-08 13:02:49 +0900

    feat: KIS 설정 및 오류 계약 추가

---
+++ back/src/main/java/com/baedang/global/clients/kis/KisClientConfiguration.java
@@ -0,0 +1,23 @@
+package com.baedang.global.clients.kis;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(KisProperties.class)
+public class KisClientConfiguration {
+
+    @Bean
+    RestClient kisRestClient(KisProperties properties) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(properties.connectTimeout());
+        requestFactory.setReadTimeout(properties.readTimeout());
+        return RestClient.builder()
+                .baseUrl(properties.baseUrl().toString())
+                .requestFactory(requestFactory)
+                .build();
+    }
+}
